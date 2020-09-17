3bd33864f3a93506dd4bbbceedd802c1f5ba4370
Jesse Chapman <jcdevwork19@gmail.com>
2020-09-17 10:14:46 +1000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const request = require('request')
+
+const app = express()
+const port = process.env.PORT || 3000
+//Define paths for exprewss config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
